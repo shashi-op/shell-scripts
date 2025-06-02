db8e614091163d7f43d86571af0d054bd6a8e634
shashi-op <skodanda777@gmail.com>
2025-06-02 07:17:05 +0530

    add

--- Disk_Usage.sh
+++ Disk_Usage.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 
+#The grep -vE 'tmp|File' command is used to exclude lines that contain either "tmp" or "File" when searching through a file or command output.
+#-d % → Specifies % as the delimiter.
+#-f1 → Extracts the first field (text before the % character)
 
 DISK_USAGE=$(df -hT | grep -vE 'tmp|File')
-echo "Disk Usage: $DISK_USAGE%"
 THRESHOLD_USAGE=1
 message=""
 
 while IFS= read -r line
 do
-    USAGE=$(echo $line | aws '{print -6F}' | cut -d % -f1)
-    PARTITION=$(echo $line | aws '{print -1F}')
+    USAGE=$(echo $line | awk '{print -6F}' | cut -d % -f1)
+    PARTITION=$(echo $line | awk '{print -1F}')
 
     if [ $USAGE - ge $THRESHOLD_USAGE ]
     then
